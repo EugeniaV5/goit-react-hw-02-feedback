c5fa58768b51b1fe67674211d6a22ec3f4256821
Ievgeniia Vashchenko <dn050493vem@gmail.com>
2022-07-25 17:25:34 +0300

    test

--- src/components/FeedbackOptions/FeedbackOptions.styled.js
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -18,7 +18,7 @@ export const Button = styled.button`
   transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
   display: inline-block;
   box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
-    7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1);
+    7px 7px 20px 0px rgba(0, 0, 0, 0.2), 4px 4px 5px 0px rgba(0, 0, 0, 0.2);
   outline: none;
 
   &:not(:last-child) {
